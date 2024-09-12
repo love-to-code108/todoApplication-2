567e0adf3af74f3d2ccd8a14dc00f07101b7b00a
love.to.code108 <love.to.code108@gmail.com>
2024-09-13 00:57:56 +0530

    making all the files needed for this project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,6 @@
-import 
+import { Routes, Route } from "react-router-dom"
+import { LandingPage } from "./Pages/landingPage"
+
 
 
 function App() {
@@ -6,6 +8,13 @@ function App() {
 
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/signup" element={<LandingPage/>} />
+        <Route path="/signin" element={<LandingPage/>} />
+        <Route path="/todoapp" element={<LandingPage/>} />
+        <Route path="/todoapp/:userId" element={<LandingPage/>} />
+      </Routes>
       
     </div>
   )
