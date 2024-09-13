16a08bbfb2ae978530e27a346c1adc33026f8329
love.to.code108 <love.to.code108@gmail.com>
2024-09-13 12:10:24 +0530

    working on the sign up page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import { LandingPage } from "./Pages/landingPage"
+import { SignUpPage } from "./Pages/SignUpPage"
 
 
 
@@ -10,7 +11,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<LandingPage/>} />
-        <Route path="/signup" element={<LandingPage/>} />
+        <Route path="/signup" element={<SignUpPage/>} />
         <Route path="/signin" element={<LandingPage/>} />
         <Route path="/todoapp" element={<LandingPage/>} />
         <Route path="/todoapp/:userId" element={<LandingPage/>} />
---
+++ Frontend/src/Pages/SignUpPage.jsx
@@ -0,0 +1,45 @@
+
+
+export const SignUpPage = () => {
+
+    return (
+        <div className=" SignUpPage-main">
+
+            {/* the sign up block */}
+            <div className="">
+
+                {/* signup block heading */}
+                <div>
+                    <h1 className=" text-5xl font-poppins mb-16">Sign Up</h1>
+                </div>
+
+
+                <div className=" font-poppins">
+                    {/* name */}
+                    <div>
+
+                        <p>Name</p>
+                        <input type="text" />
+                    </div>
+                    {/* date of birth */}
+                    <div>
+                        <p>Date of birth</p>
+                        <input type="date" />
+                    </div>
+
+                    {/* email */}
+
+                    {/* username */}
+
+                    {/* password */}
+
+                    {/* confirm password */}
+                </div>
+            </div>
+
+
+            {/* the doit logo */}
+
+        </div>
+    )
+}
