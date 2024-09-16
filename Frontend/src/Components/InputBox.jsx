4009e7ec6b6d3b177ae6c047a9426ec512a44e8f
love.to.code108 <love.to.code108@gmail.com>
2024-09-16 19:12:37 +0530

    starting with the signup page again from scratch using chakra ui this time

---
+++ Frontend/src/Components/InputBox.jsx
@@ -0,0 +1,10 @@
+import { Box, Input } from '@chakra-ui/react'
+import React from 'react'
+
+export const InputBox = () => {
+  return (
+    <Box>
+        <Input/>
+    </Box>
+  )
+}
--- Frontend/src/Pages/SignUpPage.jsx
+++ Frontend/src/Pages/SignUpPage.jsx
@@ -1,33 +1,9 @@
-import { Box, Center, Flex , Input } from "@chakra-ui/react"
+import { Box, Center, Flex, Input } from "@chakra-ui/react"
 
 
 export const SignUpPage = () => {
 
-    return (
-        <Center w={[200 , 300 , 400 , 400 , 400 , "100%"]} h={400} className=" font-poppins">
-            {/* <Box> Hello world</Box>
-            <Box> Hello world</Box>
-            <Box> Hello world</Box>
-            <Box> Hello world</Box> */}
-            <Input 
-            placeholder="Enter Something"
-            type="text" 
-            border='2px solid black'
-            focusBorderColor="black"
-            w={400}
-            sx={{
-                transition: 'transform 0.2s ease, box-shadow 0.2s ease', // Animates scaling and shadow
-                _hover: {
-                  transform: 'scale(1.05)', // Scales the input slightly
-                  boxShadow: '2px 2px 0 0 black', // Adds a shadow on focus
-                  outline: 'none', // Removes the default outline
-                },
-                // _hover:{
-                //     border: '2px solid black',
-                // },
-              }} 
-               />
-
-        </Center>
-    )
+  return (
+    
+  )
 }
