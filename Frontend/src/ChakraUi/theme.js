895d19b7513f69e5514999ca32a0e5a3a452010c
love.to.code108 <love.to.code108@gmail.com>
2024-09-16 22:12:49 +0530

    the pain is kicking in lets keep going the sign up page is completed

---
+++ Frontend/src/ChakraUi/theme.js
@@ -0,0 +1,11 @@
+// theme.js
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Poppins', sans-serif`,  // Use Poppins for headings
+    body: `'Poppins', sans-serif`,     // Use Poppins for body text
+  },
+});
+
+export default theme;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,12 +4,13 @@ import App from './App.jsx'
 import './index.scss'
 import { BrowserRouter } from "react-router-dom"
 import { ChakraProvider } from '@chakra-ui/react'
+import theme from "./ChakraUi/theme.js"
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </BrowserRouter>
