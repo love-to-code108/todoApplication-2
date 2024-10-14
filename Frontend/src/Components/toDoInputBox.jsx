b87d2137f6446c60400ef84f1137faa56ed5207a
love-to-code108 <love.to.code108@gmail.com>
2024-10-14 14:58:10 +0530

    i was able to do the todo upto a very good level i hope this continues

--- Frontend/src/Components/toDoInputBox.jsx
+++ Frontend/src/Components/toDoInputBox.jsx
@@ -1,32 +1,61 @@
 import { Input } from '@chakra-ui/react';
-import React, { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
-export const ToDoInputBox = () => {
 
-// ADDING EVENT LISTNER TO LISTEN FOR WHEN THE ENTER BUTTON IS BEING PRESSED BY THE USER
 
-useEffect(() => {
 
-    const handleKeyPress = (event) => {
-        if(event.key == 'Enter')
-            console.log("Hello");
+export const ToDoInputBox = (props) => {
 
-    };
+  console.log(props.val);
 
-    window.addEventListener('keydown',handleKeyPress);
+
+  // USING USE REF
+  const input1 = useRef()
+  
+  
+  
+  
+  // USING USE STATE
+  const [userInput, setUserInput] = useState('');
+  
+  console.log(userInput);
+  
+  
+  // HANDELING THE ENTER PRESS
+  const handleKeyPress = (event) => {
+    if (event.key == 'Enter') {
+      setUserInput(input1.current.value);
+      props.val.value = input1.current.value;
+      localStorage.setItem('userData', JSON.stringify(props.arr));
+      // console.log(input1.current.value);
+    }
+  };
+  
+  
+  
+  
+  // ADDING EVENT LISTNER TO LISTEN FOR WHEN THE ENTER BUTTON IS BEING PRESSED BY THE USER
+  
+  useEffect(() => {
+    
+    input1.current.value = props.val.value;
+    input1.current.addEventListener('keydown', handleKeyPress);
 
     // THE CLEAN UP CODE 
-    return() => {
-        window.removeEventListener('keydown',handleKeyPress);
-    } 
-},[])
+    return () => {
+      input1.current.removeEventListener('keydown', handleKeyPress);
+    }
+  }, [])
+
+
+
 
 
 
 
 
   return (
-    <Input type='text' on>
+    <Input ref={input1} type='text'>
     </Input>
   );
 }
--- Frontend/src/Pages/mainTodoPage.jsx
+++ Frontend/src/Pages/mainTodoPage.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ToDoInputBox } from '../Components/toDoInputBox'
+import { Button } from '@chakra-ui/react';
 
 export const MainTodoPage = () => {
+
+  const storedData = localStorage.getItem('userData');
+  const userData = JSON.parse(storedData);
+console.log(userData);
+
+  const [array, setArray] = useState(userData);
+
+
+  const printing = () => {
+    console.log(array);
+  }
+
+
   return (
     <div className=' w-[100vw] h-[100vh] font-poppins'>
 
-        <ToDoInputBox/>
+      {array.map((value,index) => {
+        return <ToDoInputBox key={index} val={value} arr={array}/>
+      })}
+
+      <Button onClick={printing}>Button</Button>
+
     </div>
   )
 }
